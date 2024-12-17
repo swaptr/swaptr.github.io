879d48aea620f12988b40068dc7212083471ce2e
Swapnil Tripathi <swapnil06.st@gmail.com>
2024-12-18 04:52:35 +0530

    fix urls

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment'
 
 export const title = 'Swapnil Tripathi'
 export const description = 'Swapnil Tripathi'
-export const url = dev ? 'http://localhost:5173/' : 'https://swaptr.github.io/'
+export const url = dev ? 'http://localhost:5173' : 'https://swaptr.github.io'
