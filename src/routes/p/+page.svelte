3d36c4bf1c478a441584fc82d8108e8dc177ea5c
Swapnil Tripathi <swapnil06.st@gmail.com>
2024-12-18 07:30:17 +0530

    change posts routing

--- src/routes/p/+page.svelte
+++ src/routes/p/+page.svelte
@@ -13,7 +13,7 @@
 	<ul class="posts">
 		{#each data.posts as post}
 			<li class="post">
-				<a href={`/b/${post.slug}`} class="flex flex-row items-center justify-between">
+				<a href={`/p/${post.slug}`} class="flex flex-row items-center justify-between">
 					<p class="text-lg font-bold">{post.title}</p>
 					<p class="text-muted">{formatDate(post.date)}</p>
 				</a>
