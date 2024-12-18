55b1e23a7d5879b942b16e8efe232dab11138fb2
Swapnil Tripathi <swapnil06.st@gmail.com>
2024-12-18 10:25:08 +0530

    fix build

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,16 +1,12 @@
 <script>
-	import Briefcase from '$lib/icon/briefcase.svelte';
-	import Pen from '$lib/icon/pen.svelte';
 	import Rss from '$lib/icon/rss.svelte';
-	import User from '$lib/icon/user.svelte';
 </script>
 
 <div class="flex justify-between">
 	<a href="/">Swapnil Tripathi</a>
 	<div class="flex flex-row items-center space-x-1">
-		<a href="/b"> <Pen class="h-4 w-4" /></a>
-		<a href="/p"> <Briefcase class="h-4 w-4" /></a>
-		<a href="/about"> <User class="h-4 w-4" /></a>
+		<a href="/b">Blog</a>
+		<a href="/p">Projects</a>
 		<a href="/rss.xml">
 			<Rss class="h-4 w-4" />
 		</a>
