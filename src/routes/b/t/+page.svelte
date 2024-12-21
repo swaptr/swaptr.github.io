611bcde007624397366aef54d411f7b3b0c38e6f
Swapnil Tripathi <swapnil06.st@gmail.com>
2024-12-21 12:13:09 +0530

    restyle tags

--- src/routes/b/t/+page.svelte
+++ src/routes/b/t/+page.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <Heading>Tags</Heading>
-<div class="flex flex-row flex-wrap gap-x-2 p-4">
+<div class="flex flex-col">
 	{#each Object.entries(data.tags) as [tag, count]}
-		<a class="text" href={`/b/t/${tag}`}>
+		<a class="bg-background p-1 text-center hover:brightness-150" href={`/b/t/${tag}`}>
 			{tag}<sup class="text-muted">{count}</sup>
 		</a>
 	{/each}
