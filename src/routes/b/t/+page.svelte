e2bd5bfb4fe70446fcc4e5c0bcde6b8a725c862e
Swapnil Tripathi <swapnil06.st@gmail.com>
2024-12-21 17:49:36 +0530

    fix tags screen style

--- src/routes/b/t/+page.svelte
+++ src/routes/b/t/+page.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <Heading>Tags</Heading>
-<div class="flex flex-col">
+<div class="flex flex-col md:grid md:grid-cols-3">
 	{#each Object.entries(data.tags) as [tag, count]}
-		<a class="bg-background p-1 text-center hover:brightness-150" href={`/b/t/${tag}`}>
+		<a class="bg-background p-4 text-center hover:brightness-150" href={`/b/t/${tag}`}>
 			{tag}<sup class="text-muted">{count}</sup>
 		</a>
 	{/each}
