22d82cfddeb3b526d5a0e385653e0f37f942903b
Swapnil Tripathi <swapnil06.st@gmail.com>
2024-12-21 12:33:50 +0530

    fix header and error styling

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -6,7 +6,7 @@
 <div class="flex h-6 items-center justify-between space-x-2 overflow-x-auto p-1 text-sm">
 	<a href="/" class="shrink-0 font-bold"> <Title>Swapnil Tripathi</Title></a>
 	<div class="flex flex-row items-center space-x-1">
-		<a href="/b">Blog</a>
+		<a href="/b">Posts</a>
 		<a href="/b/t">Tags</a>
 		<a href="/p">Projects</a>
 		<a href="/rss.xml">
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import Heading from '$lib/components/heading.svelte';
+
+	const { status, error } = page;
 </script>
 
-<div class="flex h-full flex-col items-center justify-center">
-	<p>{page.status}: {page.error?.message}</p>
-	<a href="/">Home</a>
+<div class="flex h-full flex-col items-center justify-center text-center">
+	<Heading>
+		{status}<br />{error?.message}
+	</Heading>
+	<nav class="flex flex-row flex-wrap justify-center gap-2 text-sm">
+		<a href="/" aria-label="Go to Home">Home↗</a>
+		<a href="/posts" aria-label="Go to Posts">Posts↗</a>
+		<a href="/projects" aria-label="Go to Projects">Projects↗</a>
+	</nav>
 </div>
